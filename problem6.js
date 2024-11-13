4355711515191e2ba167868c6ca422bdc9a9720c
Anagha <anaghamv012@gmail.com>
2024-11-14 01:12:33 +0530

    jsevaluation

--- problem6.js
+++ problem6.js
@@ -1,3 +1,5 @@
+// Question: Write a function safeParse(jsonString) that safely parses a JSON string and returns the object if valid or an error message if invalid.
+
 function safeParse(jsonString){
 
 }
