21a9b2d41c62719ff4e068571da2ecb924184c23
Anagha <anaghamv012@gmail.com>
2024-11-14 08:50:27 +0530

    jsevaluation

--- problem6.js
+++ problem6.js
@@ -10,3 +10,24 @@ const valid ='{"name": "anagha", "age":30}'
 console.log(safeParse(valid))
 const invalid ='{name: "anagha", age:30}'
 console.log(safeParse(invalid))
+
+
+
+
+
+
+
+
+
+Typing     : [43wpm][80%]
+Focus      : [4hrs 42min][1427hrs 2min]
+CT         : [02hr 59min][1295hrs 7min]
+ACT        : [20min][1040hrs 61min]
+HTML       : [00][18956]
+CSS        : [00][14197]
+JS	       : [03][10217]
+Json       : [00][1897]
+sql        : [00][70]
+TS         : [00][258]
+Total      : [03][67973]
+days       : #144
